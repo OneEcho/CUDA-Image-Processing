b1be8ec70479f9ef6e76ca412e9a05dc1d1b7405
Canyon Schwartz <canyon.schwartz@gmail.com>
2020-04-21 12:22:13 -0700

    Added different image loader and started CUDA programming

--- source/main.cu
+++ source/main.cu
@@ -1,12 +1,29 @@
-#include "include/CImg.h"
 #include <cuda.h>
 #include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "include/stb_image.h"
 
-using namespace cimg_library;
+__global__ void imgProcessingKernel(unsigned char *originalImg,
+                                    unsigned char *newImg, int width,
+                                    int height) {}
 
 int main() {
-  CImg<unsigned char> img("images/lab_puppies.jpg");
+  const char *IMG_PATH = "images/shell-image.jpg";
+  int imgWidth, imgHeight, imgPixelComponents;
 
-  img.display("My first CImg code"); // Display the image in a display window.
+  unsigned char *origImgData =
+      stbi_load(IMG_PATH, &imgWidth, &imgHeight, &imgPixelComponents, 0);
+  unsigned char *newImgData =
+      (unsigned char *)malloc(sizeof(unsigned char) * imgWidth * imgHeight);
+
+  printf("The image has a width of %d and a height of %d with %d components "
+         "per pixel. The pixel value at (128,128) is %d\n",
+         imgWidth, imgHeight, imgPixelComponents, origImgData[128]);
+
+  dim3 block(16, 16);
+  dim3 grid(imgWidth / 16, imgHeight / 16);
+  imgProcessingKernel<<<Grid, Block>>>(origImgData, newImgData, imgWidth,
+                                       imgHeight);
+
   return 0;
 }
