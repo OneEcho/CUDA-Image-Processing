8af50dc4d1e339c87296ce132b5ea73fb3134934
Canyon Schwartz <canyon.schwartz@gmail.com>
2020-04-28 20:54:59 -0700

    Restructured kernel indexing

--- source/main.cu
+++ source/main.cu
@@ -6,69 +6,74 @@
 #include "../include/stb_image_write.h"
 
 __global__ void imgProcessingKernel(unsigned char *d_origImg,
-                                    unsigned char *d_newImg, int width,
-                                    int height) {
-  // calculates the unique pixel coordinate for each thread to work on (1 pixel
-  // for 1 thread)
-  int x = (blockIdx.x * blockDim.x) + threadIdx.x;
-  int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+                                    unsigned char *d_newImg) {
+  // each thread will work on a R, G, or B value of the image
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-  unsigned char *origPixel = &d_origImg[x * width + y];
-  unsigned char *newPixel = &d_newImg[x * width + y];
-
-  newPixel[0] = origPixel[0]; // r
-  newPixel[1] = origPixel[1]; // g
-  newPixel[2] = origPixel[2]; // b
+  d_newImg[i] = d_origImg[i];
 }
 
 __host__ void imgProcessing(unsigned char *h_origImg, unsigned char *h_newImg,
-                            int width, int height) {
+                            int imgSize) {
   unsigned char *d_origImg;
   unsigned char *d_newImg;
 
   // allocate memory for the original image and new image on the device
-  cudaMalloc((void **)&d_origImg, sizeof(unsigned char) * width * height);
-  cudaMalloc((void **)&d_newImg, sizeof(unsigned char) * width * height);
+  cudaMalloc((void **)&d_origImg, imgSize);
+  cudaMalloc((void **)&d_newImg, imgSize);
 
   // copy the original image data from the host to the original image data
   // allocated on the device
-  cudaMemcpy(d_origImg, h_origImg, sizeof(unsigned char) * width * height,
-             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_origImg, h_origImg, imgSize, cudaMemcpyHostToDevice);
 
-  // setup of the block and grid
-  dim3 threadsPerBlock(8, 8);
-  dim3 numBlocks(width / threadsPerBlock.x, height / threadsPerBlock.y);
-  imgProcessingKernel<<<numBlocks, threadsPerBlock>>>(d_origImg, d_newImg,
-                                                      width, height);
+  // max amount of threads in a block is 1024
+  dim3 threadsPerBlock(1024);
+  // calculate the amount of blocks needed
+  dim3 numBlocks(imgSize / 1024);
+  // perform image processing
+  imgProcessingKernel<<<numBlocks, threadsPerBlock>>>(d_origImg, d_newImg);
   cudaThreadSynchronize();
-  cudaMemcpy(h_newImg, d_newImg, sizeof(unsigned char) * width * height,
-             cudaMemcpyDeviceToHost);
+  // copy device image to host image
+  cudaMemcpy(h_newImg, d_newImg, imgSize, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_newImg);
 }
 
+const char *IMG_PATH = "images/shell.jpg";
+
 int main() {
-  const char *IMG_PATH = "images/shell-image.jpg";
-  int imgWidth, imgHeight, imgPixelComponents;
+
+  int imgWidth, imgHeight, imgChannels;
 
   // allocate memory for original image on host
   unsigned char *h_origImg =
-      stbi_load(IMG_PATH, &imgWidth, &imgHeight, &imgPixelComponents, 0);
+      stbi_load(IMG_PATH, &imgWidth, &imgHeight, &imgChannels, 0);
+  if (h_origImg == NULL) {
+    printf("Error in loading the image\n");
+    exit(1);
+  }
+
+  // calculate the image size
+  size_t imgSize =
+      imgChannels * imgHeight * imgChannels * sizeof(unsigned char);
+
+  printf("Loaded an image with a width of %dpx, a height of %dpx and %d "
+         "channels\n",
+         imgWidth, imgHeight, imgChannels);
 
   // allocate memory for the new image on host
-  unsigned char *h_newImg =
-      (unsigned char *)malloc(sizeof(unsigned char) * imgWidth * imgHeight * 3);
-
-  // printf("The image has a width of %d and a height of %d with %d components "
-  //        "per pixel. The pixel value at (128,128) is %d\n",
-  //        imgWidth, imgHeight, imgPixelComponents, h_origImg[128]);
-  int size = sizeof(h_origImg);
-  printf("The length of the original image array is %d.\n", size);
+  unsigned char *h_newImg = (unsigned char *)malloc(imgSize);
 
   // host function to start the image processing
-  imgProcessing(h_origImg, h_newImg, imgWidth, imgHeight);
+  imgProcessing(h_origImg, h_newImg, imgSize);
   cudaDeviceSynchronize();
 
-  stbi_write_jpg("images/shell-image-copy.jpg", imgWidth, imgHeight,
-                 imgPixelComponents, h_newImg, 100);
+  // create the new image
+  stbi_write_jpg("images/shell-copy.jpg", imgWidth, imgHeight, imgChannels,
+                 h_newImg, 100);
+
+  stbi_image_free(h_origImg);
+  free(h_newImg);
 
   return 0;
 }
